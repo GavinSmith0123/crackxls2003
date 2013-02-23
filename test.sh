2554f2f9829fcf89cb07d7908e53fb203335f3de
Gavin Smith <gavinsmith0123@gmail.com>
2013-02-23 04:04:11 +0000

    Update test.sh to test a couple of extra files. (Not in repo.)

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 ./crackxls2003 -t -s "00 00 28 05 8f" protected_document.xls
+cd test-files
+../crackxls2003 -t -s "75 61 25 d0 6d" 4qwer.xls
+../crackxls2003 -t -s "00 00 00 9c de" ape\$dog.xls
