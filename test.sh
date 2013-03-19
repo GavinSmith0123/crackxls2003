aa2b240f2334ed46918d40c1a0910f652d5d18e0
Gavin Smith <gavinsmith0123@gmail.com>
2013-03-19 11:57:31 +0000

    Test decryption (files not in repo)

--- test.sh
+++ test.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+echo Testing key searching
 ./crackxls2003 -t -s "00 00 28 05 8f" protected_document.xls
 cd test-files
 ../crackxls2003 -t -s "75 61 25 d0 6d" 4qwer.xls
 ../crackxls2003 -t -s "00 00 00 9c de" ape\$dog.xls
+
+echo Testing decryption
+../crackxls2003 -d "1d da 30 05 8f" monkey.xls monkey-test.xls
+poledump monkey-test.xls
+cmp monkey-test.xls monkey-out.xls
