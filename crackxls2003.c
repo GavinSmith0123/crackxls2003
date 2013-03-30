a6bf91aeb225816fe5030bc6fab70fe1f0666c83
Gavin Smith <gavinsmith0123@gmail.com>
2013-03-30 12:04:31 +0000

    Standardize copyright and licence info

--- crackxls2003.c
+++ crackxls2003.c
@@ -1,15 +1,18 @@
-/* crackxls2003.c - recover encryption keys for Microsoft Excel 2003 worksheets
+/* Copyright (C) 2013 Gavin Smith
  *
- * Copyright (C) 2013 Gavin Smith
- * 
- * This file is distributed under the terms of the GNU General Public License
- * as published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This file is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details. */ 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
